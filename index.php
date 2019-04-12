3e3a97e22d2b4ab3c524a4bf2a24518aa0b5b112
Jason Engelbrecht <jengelbrecht2@mail.greenriver.edu>
2019-04-12 18:22:37 -0400

    Style improvements

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hola</title>
-</head>
-<body>
-
 <?php
+/*
+Jason Engelbrecht
+4.12.2019
+https://jengelbrecht.greenriverdev.com/it328/dating
+Dog dating site, route to views/home.html.
+*/
+
 //turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -32,7 +28,3 @@ $f3->route('GET /', function() {
 
 //run fat-free
 $f3->run();
-?>
-
-</body>
-</html>
