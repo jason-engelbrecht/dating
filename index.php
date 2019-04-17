6a74f39fb92ec658c2e153e2c45e72f568b6fafc
Jason Engelbrecht <jengelbrecht2@mail.greenriver.edu>
2019-04-17 17:56:30 -0400

    Add proper header/footer & display profile template

--- index.php
+++ index.php
@@ -13,8 +13,6 @@ error_reporting(E_ALL);
 //require autoload file
 require_once("vendor/autoload.php");
 
-include('views/includes/header.html');
-
 //create an instance of the base class
 $f3 = Base::instance();
 
@@ -46,7 +44,11 @@ $f3->route('GET /interests', function() {
     echo $view->render('views/sign-up/interests.html');
 });
 
-include('views/includes/footer.html');
+$f3->route('GET /display-profile', function() {
+    //display a view
+    $view = new Template();
+    echo $view->render('views/display-profile.html');
+});
 
 //run fat-free
 $f3->run();
